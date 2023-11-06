6cb33700b24ea4d2e42cb2d55c16b392c1005699
yuval <yuval@gmail.com>
2023-11-06 18:50:41 +0200

    1.0.2

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 docker build -t me-west1-docker.pkg.dev/django-project-test-404011/django-yuval/app .
-docker run -d -p 8000:8000 --name app me-west1-docker.pkg.dev/django-project-test-404011/django-yuval/app:$1
+docker run --name app -d -p 8000:8000 me-west1-docker.pkg.dev/django-project-test-404011/django-yuval/app:$1
